162f3b211258675ec86b52b32920dbfe3e2ba7b3
Marcella Brandão <marcella.pinho.brandao4@gmail.com>
2025-09-10 23:21:48 +0000

    Desafio aventureiro

--- xadrez.c
+++ xadrez.c
@@ -3,9 +3,10 @@
 int main(){
     int b = 0;
     int r = 0;
+    int movimentoCavalo = 2;
 
     //Torre
-    for (int i = 0; i < 5; i++){
+    for (int t = 0; t < 5; t++){
         printf("Torre se move para direita\n"); // Torre move 5 casas para a direita
     }
 
@@ -19,6 +20,14 @@ int main(){
     while (r < 5){
         printf("Rainha se move para esquerda\n"); // Rainha move 5 casas para a esquerda
         r++;
+    }
+
+    //cavalo
+    while(movimentoCavalo--){
+        for(int c = 0; c < 2; c++){
+            printf("Cavalo se move para cima\n"); // Cavalo move 2 casas para cima
+        }
+        printf("Cavalo se move para direita\n"); // Cavalo move 1 casa para a direita
     }
 
     return 0;
