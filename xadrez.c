7cd1da365fb7eb23291cc15255c59ecc6b48b117
Marcella Brandão <marcella.pinho.brandao4@gmail.com>
2025-09-11 19:56:07 +0000

    Nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,33 +1,60 @@
 #include <stdio.h>
 
+void moverTorre(int casas){
+    if (casas > 0){
+        printf("Torre se move para a direita.\n");
+        moverTorre(casas - 1);
+    }
+}
+
+void moverBispo(int casas){
+    if (casas > 0){
+        printf("Bispo se move para cima e direita.\n");
+        moverBispo(casas - 1);
+    }
+}
+
+void moverRainha(int casas){
+    if (casas > 0){
+        printf("Rainha se mover para a esquerda.\n");
+        moverRainha(casas - 1);
+    }
+}
+
 int main(){
     int b = 0;
     int r = 0;
     int movimentoCavalo = 2;
 
     //Torre
-    for (int t = 0; t < 5; t++){
-        printf("Torre se move para direita\n"); // Torre move 5 casas para a direita
-    }
+    moverTorre(5); // Torre move 5 casas para a direita
 
-    //Bispo
-    do{
-        printf("Bispo de move para cima e direita\n"); // Bispo move 5 casas na diagonal
-        b++;
-    } while (b < 5);
+    //Bispo recursivo
+    moverBispo(5); // Bispo move 3 casas para cima e direita
+
+    //Bispo com loops aninhados
+    for(int vertical = 0; vertical < 5; vertical++){ // Movimento vertical
+    for(int horizontal = 0; horizontal < 5; horizontal++){ // Movimento horizontal
+        if(vertical == horizontal){ // Só na diagonal
+            printf("Loops: Bispo se move para cima e direita.\n");
+        }
+    }
+}
 
     //Rainha
-    while (r < 5){
-        printf("Rainha se move para esquerda\n"); // Rainha move 5 casas para a esquerda
-        r++;
-    }
+    moverRainha(8); // Rainha move 8 casas para a esquerda
 
-    //cavalo
-    while(movimentoCavalo--){
-        for(int c = 0; c < 2; c++){
-            printf("Cavalo se move para cima\n"); // Cavalo move 2 casas para cima
+    //cavalo loops complexos
+    for (int i = 0; i <2; i++){
+        for (int j = 0; j < 3; j++){
+            if (j < 2){
+                printf("Cavalo se move para cima.\n");
+                if (j == 0) continue;
+            }else {
+                printf("Cavalo se move para a direita.\n");
+                break;
+            }
         }
-        printf("Cavalo se move para direita\n"); // Cavalo move 1 casa para a direita
     }
 
     return 0;
